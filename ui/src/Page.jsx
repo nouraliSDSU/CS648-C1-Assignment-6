13ac0c5e4e81bafe091853607e719d36cedc35ae
nouraliSDSU <nhali@sdsu.edu>
2022-04-24 21:33:06 -0700

    Move Assignment 5 over

---
+++ ui/src/Page.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import Contents from './Contents.jsx';
+
+function NavBar() {
+  return (
+    <nav>
+      <NavLink exact to="/">Home</NavLink>
+    </nav>
+  );
+}
+
+/* Navbar and the Contents */
+export default function Page() {
+  return (
+    <div>
+      <NavBar />
+      <Contents />
+    </div>
+  );
+}
